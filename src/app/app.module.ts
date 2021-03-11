65580848db87f72fb4e9b921c4525c63c8d6a140
Mahmut Karatay <mahmutkaratay61@gmail.com>
2021-03-11 22:58:08 +0300

    Angular web project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { ModelComponent } from './components/model/model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    BrandComponent,
+    NaviComponent,
+    ModelComponent
   ],
   imports: [
     BrowserModule,
